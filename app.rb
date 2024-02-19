a1236c921948128c2ad12b74a18c2299f356bff7
Alexander.Wahlström <alewah09@hotmail.com>
2024-02-19 06:24:57 +0100

    lovet v.7
    la till lite sidor och layouten

--- app.rb
+++ app.rb
@@ -3,5 +3,15 @@ require 'slim'
 require 'byebug'
 require 'sqlite3'
 require 'bcrypt'
-require_relative './model.rb' 
+#require_relative './model.rb' 
 
+get('/') do
+    db = SQLite3::Database.new("db/db.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM movies")
+    p result
+    slim(:"movies",locals:{movies:result})
+  
+  
+  
+  end
--- views/index.rb
+++
@@ -1,1 +0,0 @@
-<!-- Example view template -->
