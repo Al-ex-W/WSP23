1521ac5128fb1758788b108fb0257dbe96a3c411
Alexander.Wahlström <86449598+Al-ex-W@users.noreply.github.com>
2024-02-19 14:49:13 +0100

    19 feb -24
    la till massa funktioner och håller på med login funktionen och hur den ska samverka med sidan

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'slim'
+require 'sinatra/reloader'
 require 'byebug'
 require 'sqlite3'
 require 'bcrypt'
@@ -15,3 +16,45 @@ get('/') do
   
   
   end
+
+get('/register') do
+  slim(:register)
+end
+
+post('doregister') do
+  username = params[:username]
+  password = params[:password]
+  password_confirm = params[:password_confirm]
+
+  if (password == password_confirm)
+    password_digest = BCrypt::Password.create(password)
+    db = SQLite3::Database.new('db/db.db')
+    db.execute('INSERT INTO "users" (username,password) VALUES (?,?)',username,password_digest)
+    redirect('/')
+  else
+    "passwords did not match"
+  end
+  
+end
+
+get('/login') do
+  slim(:login)
+end
+
+
+post('/dologin') do
+  username=params[:username]
+  password=params[:password]
+  db = SQLite3::Database.new('db/todo2021.db')
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM users WHERE username = ?",username).first
+  pwdigest = result["pwdigest"]
+  id = result["id"]
+  
+  if BCrypt::Password.new(pwdigest) == password
+    session[:id] = id
+    redirect('/todos')
+  else
+    "FEL LÖSEN"
+  end
+end
