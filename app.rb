7f3f4382b39f5447054233a6612d37f468b4de56
Alexander.Wahlström <86449598+Al-ex-W@users.noreply.github.com>
2024-03-18 14:49:37 +0100

    18 mars -24
    la nästan till funktionen som låter dig skriva reviews.

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'sinatra/reloader'
 require 'byebug'
 require 'sqlite3'
 require 'bcrypt'
+enable :sessions
 #require_relative './model.rb' 
 
 get('/') do
@@ -15,13 +16,27 @@ get('/') do
   
   
   
-  end
+end
 
 get('/register') do
   slim(:register)
 end
 
-post('doregister') do
+post('/submitreview/:movieid') do 
+  #fortsätt här, jag har bara lagt in alla saker från review formen här, du ska lägga till reviewtitle i SQLITE samt fixa db.execute kommandot så att alla värden från formen kommer in korrekt i databasen.
+  movieid = params[:movieid]
+  title = params[:title]
+  reviewtext = params[:reviewtext]
+  rating = params[:rating]
+  user = session[:currentuser]
+  db = SQLite3::Database.new('db/db.db')
+  db.execute('INSERT INTO "reviews" (username,pwdigest) VALUES (?,?)',username,password_digest) #här
+  redirect('/')
+
+
+end
+
+post('/doregister') do
   username = params[:username]
   password = params[:password]
   password_confirm = params[:password_confirm]
@@ -29,7 +44,7 @@ post('doregister') do
   if (password == password_confirm)
     password_digest = BCrypt::Password.create(password)
     db = SQLite3::Database.new('db/db.db')
-    db.execute('INSERT INTO "users" (username,password) VALUES (?,?)',username,password_digest)
+    db.execute('INSERT INTO "users" (username,pwdigest) VALUES (?,?)',username,password_digest)
     redirect('/')
   else
     "passwords did not match"
@@ -41,20 +56,44 @@ get('/login') do
   slim(:login)
 end
 
+get('/movies') do
+  db = SQLite3::Database.new("db/db.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM movies")
+  p result
+  slim(:"movies",locals:{movies:result})
+
+
+
+end
+
+get('/writereview/:movieid') do
+  movieid = params[:movieid]
+  db = SQLite3::Database.new("db/db.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM movies WHERE movieid = #{movieid}")
+  slim(:writereview,locals:{reviewedmovie:result})
+end
+
 
 post('/dologin') do
   username=params[:username]
   password=params[:password]
-  db = SQLite3::Database.new('db/todo2021.db')
+  db = SQLite3::Database.new('db/db.db')
   db.results_as_hash = true
   result = db.execute("SELECT * FROM users WHERE username = ?",username).first
-  pwdigest = result["pwdigest"]
-  id = result["id"]
-  
-  if BCrypt::Password.new(pwdigest) == password
-    session[:id] = id
-    redirect('/todos')
+  if result == nil
+    "Usern finns ej"
   else
-    "FEL LÖSEN"
+    pwdigest = result["pwdigest"]
+    id = result["id"]
+    currentuser = result["username"]
+    if BCrypt::Password.new(pwdigest) == password
+      session[:id] = id
+      session[:currentuser] = currentuser
+      redirect('/')
+    else
+      "FEL LÖSEN"
+    end
   end
 end
